c387f955f1e42f489507041b9b15dee2e4b13e41
stofstik <verstegen.daan@gmail.com>
2016-12-31 15:28:44 +0100

    more pixel fucking

--- imports/ui/components/Message.jsx
+++ imports/ui/components/Message.jsx
@@ -2,6 +2,11 @@ import React, { Component, PropTypes } from 'react';
 import moment from 'moment';
 
 export default class Message extends Component {
+  componentDidMount() {
+    // This user has 'seen' this message
+    // The message object in the db will get an array of userids who have read it
+  }
+
   belongsToCurrentUser() {
     return this.props.message.owner === this.props.currentUser._id;
   }
@@ -14,8 +19,12 @@ export default class Message extends Component {
     return (
       <div className="message-container" style={this.alignment()}>
         <div className="message">
-          <span className="text">{ this.props.message.text }</span>
-          <span className="time">{ moment(this.props.message.createdAt).format('MMM Do HH:mm:ss') }</span>
+          <div className="text">
+            <span>{ this.props.message.text }</span>
+          </div>
+          <div className="time">
+            <span>{ moment(this.props.message.createdAt).format('MMM Do HH:mm:ss') }</span>
+      </div>
       </div>
       </div>
     );
